c6b04e1bd8bb8c15327f54e2b36641a30ac087ed
Tyson-Zwicker <tysonzwicker@gmail.com>
2021-10-11 15:09:04 -0700

    a fake app just to for the apps folder to show up.

---
+++ apps/placeholderapp.mjs
@@ -0,0 +1,11 @@
+export function getPage(){
+	var s='';
+	var t = '\t'
+	var r = '\r\n';
+	s+= '<html>'+r;
+	s+= t+'<body>'+r;
+	s+= t+t+'Place holder app.'+r;
+	s+= t+'</body>';
+	s+= 'html';
+	return s;
+}
